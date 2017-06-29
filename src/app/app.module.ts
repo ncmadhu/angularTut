7777e7ce31fe4ad999e8d14488c6faf4ec416657
Madhu Chakravarthy <ncmadhu@gmail.com>
2017-06-29 18:51:50 +0530

    Adding new files to repo

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent }  from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent }  from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { HeroSearchComponent }  from './hero-search.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
+// Imports for mock service
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+@NgModule({
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
+  declarations: [ AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent, HeroSearchComponent ],
+  bootstrap:    [ AppComponent ],
+  providers:    [ HeroService ]
+})
+export class AppModule { }
